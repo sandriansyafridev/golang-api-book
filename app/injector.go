39cb5321c260fd8a41693a78a3f14303cb7453a7
sandriansyafridev <sandriansyafri.dev@gmail.com>
2022-04-26 19:21:58 +0700

    refactor manual depedency to auto depedency injection

---
+++ app/injector.go
@@ -0,0 +1,31 @@
+package app
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/google/wire"
+	"github.com/sandriansyafridev/golang/api/book/handler"
+	"github.com/sandriansyafridev/golang/api/book/repository"
+	"github.com/sandriansyafridev/golang/api/book/service"
+)
+
+var (
+	BookSet = wire.NewSet(
+		wire.Bind(new(repository.BookRepository), new(*repository.BookRepositoryImpl)),
+		wire.Bind(new(service.BookService), new(*service.BookServiceImpl)),
+		wire.Bind(new(handler.BookHandler), new(*handler.BookHandlerImpl)),
+		repository.NewBookRepositoryImpl,
+		service.NewBookServiceImpl,
+		handler.NewBookHandlerImpl,
+	)
+)
+
+func InitializedRouter() *gin.Engine {
+
+	wire.Build(
+		NewDatabase,
+		NewRouter,
+		BookSet,
+	)
+
+	return nil
+}
--- main.go
+++ main.go
@@ -1,22 +1,10 @@
 package main
 
-import (
-	"github.com/sandriansyafridev/golang/api/book/app"
-	"github.com/sandriansyafridev/golang/api/book/handler"
-	"github.com/sandriansyafridev/golang/api/book/repository"
-	"github.com/sandriansyafridev/golang/api/book/service"
-)
-
-var (
-	db             = app.NewDatabase()
-	bookRepository = repository.NewBookRepositoryImpl(db)
-	bookService    = service.NewBookServiceImpl(bookRepository)
-	bookHandler    = handler.NewBookHandlerImpl(bookService)
-)
+import "github.com/sandriansyafridev/golang/api/book/app"
 
 func main() {
-	defer app.CloseDatabase(db)
 
-	r := app.NewRouter(bookHandler)
+	r := app.InitializedRouter()
 	r.Run(":8080")
+
 }
